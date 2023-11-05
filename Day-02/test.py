d33c5aab2da500a173000f07ed33ac8a454c4b97
rejunath <127033083+rejunath@users.noreply.github.com>
2023-11-05 15:21:00 +0530

    Update test.py

--- Day-02/test.py
+++ Day-02/test.py
@@ -12,3 +12,13 @@ txt1 = "My name is {fname}, I'm {old}".format(fname = name, old = age)
 print(txt1)
 ##reference for Reg expression : https://www.w3schools.com/python/python_regex.asp
 ##pattern r("") ---> r stands for regular expression
+server_name = "my_server"
+port = 80
+is_https_enabled = True
+max_connections = 1000
+
+# Print the configuration
+print(f"Server Name: {server_name}")
+print(f"Port: {port}")
+print(f"HTTPS Enabled: {is_https_enabled}")
+print(f"Max Connections: {max_connections}")
